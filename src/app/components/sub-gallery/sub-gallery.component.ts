e46d89ddb495824b45f6dd76949ef177e169ea32
mikkomajamaa <mikko.majamaa@outlook.com>
2019-09-20 15:18:44 +0300

    updates on sub-gallery frontend

--- src/app/components/sub-gallery/sub-gallery.component.ts
+++ src/app/components/sub-gallery/sub-gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Image } from '../../interfaces/image';
 
 // services
 import { ImagesService } from '../../services/images.service';
@@ -11,11 +12,14 @@ import { ImagesService } from '../../services/images.service';
 })
 export class SubGalleryComponent implements OnInit {
   public images: Image[];
+  public gallery: any;
+  public slideIndex;
 
   constructor(private activatedRoute: ActivatedRoute,
               private imagesService: ImagesService) { }
 
   ngOnInit() {
+    this.slideIndex = 1;
     // filter images based on gallery's name
     this.gallery = this.activatedRoute.params.subscribe(params => {
       this.imagesService.getImages().subscribe(res => {
@@ -28,12 +32,47 @@ export class SubGalleryComponent implements OnInit {
     e.style.display = "none";
   }
 
+  openModal() {
+    console.log('ju')
+    document.getElementById("myModal").style.display = "block";
+  }
+
+  closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      (<HTMLElement>slides[i]).style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    (<HTMLElement>slides[this.slideIndex-1]).style.display = "block";
+    dots[this.slideIndex-1].className += " active";
+    captionText.innerHTML = (<HTMLImageElement>dots[this.slideIndex-1]).alt;
+  }
+
   @HostListener('click', ['$event.target']) clickInside(e) {
     if (e.id == "expanded-img" || e.className != "gallery-img") return
 
-    let expandImg = document.getElementById("expanded-img");
+    let expandImg = <HTMLElement>document.getElementById("expanded-img");
     // get the image text
-    var imgText = document.getElementById("img-text");
+    var imgText = <HTMLElement>document.getElementById("img-text");
     // use the same src in the expanded image as the image being clicked on from the grid
     (<HTMLImageElement>expandImg).src = e.src;
     // use the value of the alt attribute of the clickable image as text inside the expanded image
