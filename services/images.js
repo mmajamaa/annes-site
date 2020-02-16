c8d0d705274c4d42609a88dfb411bb78feefb5a1
mikkomajamaa <mikko.majamaa@outlook.com>
2020-02-16 14:50:44 +0200

    only load config.json on dev

--- services/images.js
+++ services/images.js
@@ -1,63 +1,67 @@
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
-const config = require('../config')
+if (process.env !== "production") {
+  const config = require("../config.json");
+}
 
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS || config.AWS_SECRET_ACCESS,
   accessKeyId: process.env.AWS_ACCESS_KEY || config.AWS_ACCESS_KEY,
-  region: 'us-east-2'
-})
+  region: "us-east-2"
+});
 
 const s3 = new aws.S3();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
-    cb(new Error('Invalid Mime Type, only JPEG and PNG'), false);
+    cb(new Error("Invalid Mime Type, only JPEG and PNG"), false);
   }
-}
+};
 
 const upload = multer({
   fileFilter,
   storage: multerS3({
     s3,
-    bucket: 'annes-gallery',
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: 'TESTING'});
+    bucket: "annes-gallery",
+    acl: "public-read",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: "TESTING" });
     },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString())
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
     }
   })
-})
+});
 
 const deleteImage = file => {
   s3.deleteObject(
     {
-      Bucket: 'annes-gallery',
+      Bucket: "annes-gallery",
       Key: file
-    }, (error, data) => {
+    },
+    (error, data) => {
       if (error) console.log(error, error.stack);
     }
   );
-}
+};
 
 const deleteImages = files => {
   s3.deleteObjects(
     {
-      Bucket: 'annes-gallery',
+      Bucket: "annes-gallery",
       Delete: {
-        Objects: files,
+        Objects: files
       }
-  },
-  function(error, data) {
-    if (error) console.log(error, error.stack);
-    else console.log('delete', data);
-});
-}
+    },
+    function(error, data) {
+      if (error) console.log(error, error.stack);
+      else console.log("delete", data);
+    }
+  );
+};
 
-module.exports = {upload, deleteImage, deleteImages};
+module.exports = { upload, deleteImage, deleteImages };
