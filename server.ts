069c6308b2ec63d951c5b56b8bc75e5e79448219
mikkomajamaa <mikko.majamaa@outlook.com>
2020-02-25 10:09:02 +0200

    add middleware to force https

--- server.ts
+++ server.ts
@@ -11,6 +11,20 @@ if (process.env.NODE_ENV !== "production") {
   config = require("./config.json");
 }
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 app.use(express.static(path.join(__dirname, "dist/anne/")));
 
 app.use(express.json());
