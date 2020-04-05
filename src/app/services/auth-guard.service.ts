ad32e74f6342cba30d825a4994d88407b9c41e33
mikkomajamaa <mikko.majamaa@outlook.com>
2020-04-05 14:03:17 +0300

    remove console logs

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -6,13 +6,9 @@ import { map, take, catchError } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-    constructor(private authenticationService: AuthenticationService, private router: Router, private route: ActivatedRoute) {
-        console.log(this.route.snapshot.queryParams['testi'])
-    }
+    constructor(private authenticationService: AuthenticationService, private router: Router, private route: ActivatedRoute) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        console.log(this.route.snapshot.queryParams['testi'])
-
         return this.authenticationService.authStatus().pipe(map(
             data => {
                 return true;
