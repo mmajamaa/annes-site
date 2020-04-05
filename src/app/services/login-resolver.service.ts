d59930b7e1fc2d958c29a1f399a286eca361c5b0
mikkomajamaa <mikko.majamaa@outlook.com>
2020-04-05 14:06:51 +0300

    after logout don't try to auto login

--- src/app/services/login-resolver.service.ts
+++ src/app/services/login-resolver.service.ts
@@ -9,6 +9,10 @@ export class LoginResolver implements Resolve<any> {
     constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (route.queryParams.resolve === 'false') {
+            return;
+        }
+        
         return this.authenticationService.authStatus().subscribe(data => {
             this.router.navigate(['admin'])
             return data
