55e169b8b16c8b28886ae4ece6332e7f5019fb41
mikkomajamaa <mikko.majamaa@outlook.com>
2020-05-09 08:29:24 +0300

    add first tests

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() { }
 
-  loginUser(form: NgForm) {
+  loginUser(form) {
     const username = form.value.username;
     const password = form.value.password;
 
