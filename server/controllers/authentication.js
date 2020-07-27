0dcc23775a62d32c32f618d97dcd3bb6dabfd196
mikkomajamaa <mikko.majamaa@outlook.com>
2020-07-27 09:43:26 +0300

    move jwt secret to conf file

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const helpers = require("./helpers");
-
 const User = require("../models/users");
+
+let config = { secret: "" };
+
+if (process.env.NODE_ENV !== "production") {
+  config = require("../config.json");
+}
 
 module.exports = {
   index: async (req, res, next) => {
@@ -16,11 +21,15 @@ module.exports = {
       if (doc) {
         if (doc.isValid(password)) {
           // generate token
-          let token = jwt.sign({ username }, process.env.secret || "secret", {
-            expiresIn: "3d"
-          });
+          let token = jwt.sign(
+            { username },
+            process.env.secret || config.secret,
+            {
+              expiresIn: "3d",
+            }
+          );
           console.log("succesful login");
-          return res.status(200).json({token});
+          return res.status(200).json({ token });
         } else {
           console.log("invalid pw");
           return res.status(501).json({ message: "Invalid password." });
@@ -39,7 +48,7 @@ module.exports = {
     return;
     let user = new User({
       username: req.body.username,
-      password: User.hashPassword(req.body.password)
+      password: User.hashPassword(req.body.password),
     });
 
     let doc = await user.save();
@@ -52,6 +61,6 @@ module.exports = {
   },
 
   authenticated: async (req, res, next) => {
-    return res.status(200).json({message: "User is authenticated."});
-  }
+    return res.status(200).json({ message: "User is authenticated." });
+  },
 };
--- server/controllers/helpers/index.js
+++ server/controllers/helpers/index.js
@@ -1,15 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+
+let config = { secret: "" };
+
+if (process.env.NODE_ENV !== "production") {
+  config = require("../../config.json");
+}
 
 module.exports = {
   verifyToken: (req, res, next) => {
     let token = req.query.token;
-    jwt.verify(token, process.env.secret || 'secret', (err, tokendata) => {
+    jwt.verify(token, process.env.secret || config.secret, (err, tokendata) => {
       if (err) {
-        return res.status(400).json({message: 'Unauthorized request'});
+        return res.status(400).json({ message: "Unauthorized request" });
       }
       if (tokendata) {
         next();
       }
-    })
-  }
-}
+    });
+  },
+};
