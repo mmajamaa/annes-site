1df979093374a402ecc8485ea813bf49d55234ba
mikkomajamaa <mikko.majamaa@outlook.com>
2020-08-15 17:14:29 +0300

    move anne's img to s3

--- src/app/components/public/home/home.component.ts
+++ src/app/components/public/home/home.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { TranslationsService } from "../translations.service";
-import { Subscription } from 'rxjs';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   public I18n: any;
   private I18nSubscription: Subscription;
 
-  constructor(private translationsService: TranslationsService) { }
+  constructor(private translationsService: TranslationsService) {}
 
   ngOnInit() {
-    this.I18nSubscription = this.translationsService.I18n.subscribe(r => { this.I18n = r });
+    this.I18nSubscription = this.translationsService.I18n.subscribe((r) => {
+      this.I18n = r;
+    });
   }
 
   ngOnDestroy() {
