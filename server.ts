6f5cce824d9fd3dfcd9ea3f137b365de7da45413
mikkomajamaa <mikko.majamaa@outlook.com>
2020-08-22 09:17:54 +0300

    don't require https on develop

--- server.ts
+++ server.ts
@@ -12,12 +12,12 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 function requireHTTPS(req, res, next) {
+  if (process.env.NODE_ENV !== "production") {
+    return next();
+  }
+
   // The 'x-forwarded-proto' check is for Heroku
-  if (
-    !req.secure &&
-    req.get("x-forwarded-proto") !== "https" &&
-    process.env.NODE_ENV !== "development"
-  ) {
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
     return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
