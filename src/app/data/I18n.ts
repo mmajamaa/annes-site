2cbcc7ac9b7007079de5650c78825e048383ad3f
mikkomajamaa <mikko.majamaa@outlook.com>
2020-10-17 19:21:19 +0300

    start working on generalization of the app

--- src/app/data/I18n.ts
+++ src/app/data/I18n.ts
@@ -19,6 +19,166 @@ export const fi = {
   page_not_found: "Sivua ei löytynyt.",
   go_home: "Mene etusivulle.",
   gallery_empty: "Tämä alagalleria on tyhjä.",
+  cv_table: `<tr>
+  <td>CV</td>
+  <td>Anne Puronmaa s. 04.08.1968</td>
+</tr>
+
+<tr class="space">
+  <td>Taidekoulutus:</td>
+  <td>Sisälähetysseuran opisto, taito- ja taideaineiden linja 1997-1998</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan ammattikorkeakoulu, kuvataiteen yksikkö 2003-2007</td>
+</tr>
+<tr class="space">
+  <td>Yhteisnäyttelyt:</td>
+  <td>Pieksämäen kuvataideseuran vuosinäyttely 1999</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Wanhat Weturitallit, Pieksämäki 2000</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Varkauden taidemuseo 2000</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>PAND-rauhankulkuenäyttely, Helsinki 2002</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Imatran kaupunginteatteri, Imatra 2005</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Stockforss, Pyhtää 2006</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Imatran taidemuseo, Imatra 2007</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Kaapelitehdas, Helsinki 2007</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidetalo Kalliopeia, Savonlinna 2007</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2008</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>USA, kiertävä näyttely Atlantan alueella 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Imatran taidemuseo 2010</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Human Culture Imatra 2010</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2013</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Antares 2015</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Katariina, Lappeenrannan linnoitus 2015</td>
+</tr>
+<tr>
+  <td></td>
+  <td>5000 m2 Lappeenranta 2016</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Vihreä makasiini, Lappeenrannan linnoitus 2016</td>
+</tr>
+<tr class="space">
+  <td>Teoksia kokoelmissa:</td>
+  <td>Henna ja Pertti Niemistö nykytaiteen kokoelma</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Merikaarinan Syöpäyhdistys, Turku</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan Ammattikorkeakoulu</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Mäntyharjun kunta</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan taidemuseo</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Hämeenlinnan taidemuseo</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Suomen Sisälähetysseuran opisto</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Imatran Taidemuseo</td>
+</tr>
+<tr>
+  <td class="space">Jäsenyydet:</td>
+  <td>Taidemaalariliitto, kokelasjäsen</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-karjalan Taiteilijaseura ry hallituksen jäsen 2015-</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Kuvasto</td>
+</tr>
+<tr class="space">
+  <td>Apurahat:</td>
+  <td>Kaakkois-Suomen taidetoimikunta, työskentelyapuraha ½ v. 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>
+    Suomen Kulttuurirahaston Etelä-Karjalan rahasto, työskentelyapuraha 1v.
+    2010
+  </td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+</table>
+  `,
 };
 
 export const en = {
@@ -37,4 +197,164 @@ export const en = {
   page_not_found: "Page not found.",
   go_home: "Go to homepage",
   gallery_empty: "This sub-gallery is empty.",
+  cv_table: `<tr>
+  <td>CV</td>
+  <td>Anne Puronmaa s. 04.08.1968</td>
+</tr>
+
+<tr class="space">
+  <td>Taidekoulutus:</td>
+  <td>Sisälähetysseuran opisto, taito- ja taideaineiden linja 1997-1998</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan ammattikorkeakoulu, kuvataiteen yksikkö 2003-2007</td>
+</tr>
+<tr class="space">
+  <td>Yhteisnäyttelyt:</td>
+  <td>Pieksämäen kuvataideseuran vuosinäyttely 1999</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Wanhat Weturitallit, Pieksämäki 2000</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Varkauden taidemuseo 2000</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>PAND-rauhankulkuenäyttely, Helsinki 2002</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Imatran kaupunginteatteri, Imatra 2005</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Stockforss, Pyhtää 2006</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Imatran taidemuseo, Imatra 2007</td>
+</tr>
+
+<tr>
+  <td></td>
+  <td>Kaapelitehdas, Helsinki 2007</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidetalo Kalliopeia, Savonlinna 2007</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2008</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>USA, kiertävä näyttely Atlantan alueella 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Imatran taidemuseo 2010</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Human Culture Imatra 2010</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Salmela 2013</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Taidekeskus Antares 2015</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Katariina, Lappeenrannan linnoitus 2015</td>
+</tr>
+<tr>
+  <td></td>
+  <td>5000 m2 Lappeenranta 2016</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Vihreä makasiini, Lappeenrannan linnoitus 2016</td>
+</tr>
+<tr class="space">
+  <td>Teoksia kokoelmissa:</td>
+  <td>Henna ja Pertti Niemistö nykytaiteen kokoelma</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Merikaarinan Syöpäyhdistys, Turku</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan Ammattikorkeakoulu</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Mäntyharjun kunta</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-Karjalan taidemuseo</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Hämeenlinnan taidemuseo</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Suomen Sisälähetysseuran opisto</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Imatran Taidemuseo</td>
+</tr>
+<tr>
+  <td class="space">Jäsenyydet:</td>
+  <td>Taidemaalariliitto, kokelasjäsen</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Etelä-karjalan Taiteilijaseura ry hallituksen jäsen 2015-</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Kuvasto</td>
+</tr>
+<tr class="space">
+  <td>Apurahat:</td>
+  <td>Kaakkois-Suomen taidetoimikunta, työskentelyapuraha ½ v. 2009</td>
+</tr>
+<tr>
+  <td></td>
+  <td>
+    Suomen Kulttuurirahaston Etelä-Karjalan rahasto, työskentelyapuraha 1v.
+    2010
+  </td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+</table>
+  `,
 };
